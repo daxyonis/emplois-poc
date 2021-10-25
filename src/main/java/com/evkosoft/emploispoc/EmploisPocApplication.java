f2303bbef00c19da1db6ebd56bfa727b7ac5cfc1
daxyonis <emaciejko@gmail.com>
2021-10-25 17:54:20 -0400

    Initial commit with both backend and frontend scaffolding

---
+++ src/main/java/com/evkosoft/emploispoc/EmploisPocApplication.java
@@ -0,0 +1,13 @@
+package com.evkosoft.emploispoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploisPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploisPocApplication.class, args);
+	}
+
+}
